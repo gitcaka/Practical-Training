4a6e7abc3a36b875077479ecb2e03180e264161a
gitcaka <2217351805@qq.com>
2024-01-09 15:53:43 +0800

    Changes

---
+++ 小测验1-绝对温标.py
@@ -0,0 +1,2 @@
+centigrade = input('请输入摄氏度(℃)：')
+print('开式度为 ' + str(float(centigrade) + 273.15))
---
+++ 小测验2-体质指数.py
@@ -0,0 +1,3 @@
+weight = input('请输入体重(kg)：')
+heght = input('请输入身高(m)：')
+print('BMI为 ' + str(float(weight) / float(heght) * 2))
